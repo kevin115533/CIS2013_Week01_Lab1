e9a7c6e56da241c9ba7aacdbac6fe2cb7d85eeb9
kevin115533 <35547586+kevin115533@users.noreply.github.com>
2018-01-17 20:39:25 -0600

    First program.

---
+++ helloworld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	char something;
+	cout << "Type a characer and hit enter";
+	cin >> something;
+	cout << "You typed " << something;
+	return 0;
+}
